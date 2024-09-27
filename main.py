737d139b2e78bfab81fa46bc5d9cf8d9b9bb50cc
Bernardomeirelles <bernardo.meirelles@sou.inteli.edu.br>
2024-09-27 13:11:17 -0300

    add: project

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# This entrypoint file to be used in development. Start by reading README.md
+# Arquivo de entrada para desenvolvimento. Certifique-se de ler o README.md
 import mean_var_std
 from unittest import main
 
+# Testando a função diretamente com a entrada fornecida
 print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
 
-# Run unit tests automatically
+# Rodar os testes automaticamente
 main(module='test_module', exit=False)
